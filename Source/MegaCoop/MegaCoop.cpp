a8737ba738d7821133aa09c135daceb7cd0b3e4e
Krexonn <yunusgoker97@gmail.com>
2026-01-16 14:48:42 +0300

    Ilk yukleme

---
+++ Source/MegaCoop/MegaCoop.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MegaCoop.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MegaCoop, "MegaCoop" );
