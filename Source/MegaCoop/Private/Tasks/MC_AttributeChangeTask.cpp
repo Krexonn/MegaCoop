d93f32137616b9d5bbdd20906fc72191ccd1273b
Krexonn <yunusgoker97@gmail.com>
2026-02-09 12:35:45 +0300

    Added Async Attribute Change Task

---
+++ Source/MegaCoop/Private/Tasks/MC_AttributeChangeTask.cpp
@@ -0,0 +1,41 @@
+﻿// Copyright Krexonn
+
+#include "Tasks/MC_AttributeChangeTask.h"
+#include "AbilitySystemComponent.h"
+
+
+
+
+UMC_AttributeChangeTask* UMC_AttributeChangeTask::ListenForAttributeChange(UAbilitySystemComponent* AbiltiySystemComponent, FGameplayAttribute Attribute)
+{
+	UMC_AttributeChangeTask* WaitForAttributeChangeTask = NewObject<UMC_AttributeChangeTask>();
+	WaitForAttributeChangeTask->ASC = AbiltiySystemComponent;
+	WaitForAttributeChangeTask->AttributeToListenFor = Attribute;
+
+	if (!IsValid(AbiltiySystemComponent))
+	{
+		WaitForAttributeChangeTask->RemoveFromRoot();
+		return nullptr;
+	}
+
+	AbiltiySystemComponent->GetGameplayAttributeValueChangeDelegate(Attribute).AddUObject(WaitForAttributeChangeTask, &UMC_AttributeChangeTask::AttributeChanged);
+
+	return WaitForAttributeChangeTask;
+}
+
+void UMC_AttributeChangeTask::EndTask()
+{
+	if (ASC.IsValid())
+	{
+		ASC->GetGameplayAttributeValueChangeDelegate(AttributeToListenFor).RemoveAll(this);
+	}
+
+	SetReadyToDestroy();
+	MarkAsGarbage();
+	
+}
+
+void UMC_AttributeChangeTask::AttributeChanged(const FOnAttributeChangeData& Data)
+{
+	OnAttributeChanged.Broadcast(Data.Attribute, Data.NewValue, Data.OldValue);
+}
---
+++ Source/MegaCoop/Public/Tasks/MC_AttributeChangeTask.h
@@ -0,0 +1,32 @@
+﻿// Copyright Krexonn
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "MC_AttributeChangeTask.generated.h"
+
+struct FOnAttributeChangeData;
+class UAbilitySystemComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnAttributeChanged, FGameplayAttribute, Attribute, float, NewValue, float, OldValue);
+UCLASS(BlueprintType, meta = (ExposedAsyncProxy = AsyncTask))
+class MEGACOOP_API UMC_AttributeChangeTask : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChanged OnAttributeChanged;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UMC_AttributeChangeTask* ListenForAttributeChange(UAbilitySystemComponent* AbiltiySystemComponent, FGameplayAttribute Attribute);
+
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+
+	TWeakObjectPtr<UAbilitySystemComponent> ASC;
+	FGameplayAttribute AttributeToListenFor;
+
+	void AttributeChanged(const FOnAttributeChangeData& Data);
+};
